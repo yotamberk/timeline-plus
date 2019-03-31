6ce88ad9eab1ba5ba094b2c6063feb780d7b5142
Matthew Strasiotto <39424834+mstr3336@users.noreply.github.com>
2019-04-01 10:16:25 +1100

    Added Unit Tests, UI Tests, jest and testcafe (#6)
    * Added testcafe to package.json
    
    * Added jest to dependencies
    
    * .DS store added to gitignore
    
    * Added testcafe runner to scripts
    
    * Added UI test for nesting
    
    * Added test runner to scripts
    
    * Added test for group order
    
    * Update test_multinest.js

---
+++ tests/UI/test_multinest.js
@@ -0,0 +1,24 @@
+import { Selector } from 'testcafe';
+
+fixture `nesting`.page `./multi_nest.html`;
+
+test('Is correct nested order restored?', async t => {
+	const groups = Selector('.timeline-label');
+	const nesting = groups.withText('1');
+	const nested  = groups.withText('3');
+	
+	await t
+		.expect(groups.count).eql(3)
+		.click(nesting);
+
+	await t
+	.expect(groups.count).eql(2);
+	
+	await t.expect(nested.exists).notOk()
+	
+	await t.click(nesting);
+	
+	await t
+	.expect(nesting.nextSibling(0).textContent)
+	.contains('3');
+})
